944a4bad05d95fe3e6b4324d4b0786f9b73c408d
席智欣 <947538363@qq.com>
2018-10-26 14:33:06 +0800

    使用redux实现数据渲染

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 // import App from './App';
 // import ClickCounter from './ClickCounter';
 // import ControlPanel from './chapter02/ControlPanel';
 // import ControlPanel from './chapter03/flux/views/ControlPanel';
-import ControlPanel from './chapter03/redux/views/ControlPanel';
-
+// import ControlPanel from './chapter03/redux/views/ControlPanel';
+import TodoApp from './chapter04/TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <ControlPanel />, document.getElementById('root')
+    <Provider>
+        <TodoApp />
+    </Provider>, document.getElementById('root')
 );
 registerServiceWorker();
